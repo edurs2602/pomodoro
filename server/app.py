3a6016ab59ec4e8647653e21a9f588323c00506f
joapedu <joaoeduardobraga2@gmail.com>
2023-09-22 16:34:03 -0300

    feat( #7 ): new server to dockering project, almost done

---
+++ server/app.py
@@ -0,0 +1,42 @@
+import time
+
+import redis
+
+from crypt import methods
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+cadastros = []
+registros = []
+
+@app.route('/', methods=["GET", "POST"])
+def principal():
+    if request.method == "POST":
+        if request.form.get("cadastro"):
+            cadastros.append(request.form.get("cadastro"))
+    return render_template("index.html", cadastros=cadastros)
+
+@app.route('/sobre', methods=["GET", "POST"])
+def sobre():
+    if request.method == "POST":
+        if request.form.get("aluno") and request.form.get("nota"):
+            registros.append({"aluno": request.form.get("aluno"),
+            "nota": request.form.get("nota")})
+    return render_template("sobre.html", registros=registros)
+
+if __name__ =="__main__":
+    app.run(debug=True)
